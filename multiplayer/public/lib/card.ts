ee92896e849cd7792bace317be29a771942cf7ef
mbxtn <markbxtn@gmail.com>
2025-09-14 10:24:12 -0700

    reformat card.ts a little bit

--- multiplayer/public/lib/card.ts
+++ multiplayer/public/lib/card.ts
@@ -1,22 +1,31 @@
 // Class representing cards, will provide game state
 // that can be built on per clients
 export class Card {
-    cardName: string; 
-    id: string;
+
+    // Basic information about a card
+    // this should be copied if we make duplicates
+    cardName: string;
     scryfallId: string;
+    frontFace?: { name: string, imageUris: string[] };
+    backFace?: { name: string, imageUris: string[] };
+
+    // Information more specfic to a player themselves,
+    // this should probably be preserved betweem games.
+    commander: boolean;
+    id: string;
+
+    // Informatiion that's extemely stateful
     tapped: boolean;
     flipped: boolean;
     counters = 0;
 
-    frontFace?: {name: string, imageUris: string[]};
-    backFace?: {name: string, imageUris: string[]};
-
-    constructor(id: string, name: string, scryfallData?: any) {
+    constructor(id: string, name: string,scryfallData?: any, isCommander = false) {
         this.cardName = name;
         this.id = id;
         this.scryfallId = "";
         this.tapped = false;
         this.flipped = false;
+        this.commander = isCommander;
     }
 
     toggleTap() {
