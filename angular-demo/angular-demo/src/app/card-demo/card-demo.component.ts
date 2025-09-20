a5afc11a1a0ed036ef723b382c13636ea8a804e5
mbxtn <markbxtn@gmail.com>
2025-09-20 01:26:42 -0700

    style it up a little more like a battle field

--- angular-demo/angular-demo/src/app/card-demo/card-demo.component.ts
+++ angular-demo/angular-demo/src/app/card-demo/card-demo.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, CdkDragEnd, CdkDragRelease, CdkDragStart, moveItemInArray } from '@angular/cdk/drag-drop';
 import { MatCardModule } from '@angular/material/card';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
+import { P } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-card-demo',
@@ -15,19 +16,61 @@ export class CardDemoComponent {
   cards = [
     {
       name: 'Gumdrop Poisoner',
-      image: 'https://cards.scryfall.io/normal/front/5/c/5cb01d4d-91c2-41c6-981e-b4135a1e1e36.jpg?1692937659'
+      image: 'https://cards.scryfall.io/normal/front/5/c/5cb01d4d-91c2-41c6-981e-b4135a1e1e36.jpg?1692937659',
+      tapped: false,
+      dragging: false,
+      zIndex: 1,
     },
     {
       name: 'Lightning Bolt',
-      image: 'https://cards.scryfall.io/normal/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721'
+      image: 'https://cards.scryfall.io/normal/front/c/e/ce711943-c1a1-43a0-8b89-8d169cfb8e06.jpg?1628801721',
+      tapped: false,
+      dragging: false,
+      zIndex: 2,
     },
     {
       name: 'Counterspell',
-      image: 'https://cards.scryfall.io/normal/front/3/6/36f9d5b0-51a5-4faa-a879-b83871ae39cc.jpg?1757550235'
+      image: 'https://cards.scryfall.io/normal/front/3/6/36f9d5b0-51a5-4faa-a879-b83871ae39cc.jpg?1757550235',
+      tapped: false,
+      dragging: false,
+      zIndex: 3,
     }
   ];
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.cards, event.previousIndex, event.currentIndex);
+  activeCardIndex: number | null = null;
+  nextZIndex: number = 1;
+
+  ngOnInit() {
+    // Initialize zIndex for each card
+    this.cards.forEach((card, i) => card.zIndex = i + 1);
+    this.nextZIndex = this.cards.length + 1;
+  }
+
+  onCardClick(card: any, e: PointerEvent) {
+    console.log("tapping,", e);
+    // nothing to do if the card is dragging
+    if(card.dragging) {
+      return;
+    }
+    
+    card.tapped = !card.tapped;
+  }
+
+  drag(card: any, e: CdkDragStart<any>, index: number) {
+    console.log("drag start:", e);
+    card.dragging = true;
+    card.zIndex = this.nextZIndex++;
+    this.activeCardIndex = index;
+  }
+
+  drop(card: any, e: CdkDragEnd<any>) {
+    console.log("drag end:", e);
+    setTimeout(() => {card.dragging = false;}, 10);
+    //e.event.stopPropagation();
+  }
+
+  released(e: CdkDragRelease) {
+    console.log("release: ", e);
+    //e.event.stopPropagation();
   }
 }
